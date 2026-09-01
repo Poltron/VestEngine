dca8912648604fb5675b09579d5a3caf676ddf17
Etienne Champagne <etienne.champagne.ec@gmail.com>
2026-09-02 00:02:56 +0200

    add: wip test framework

--- VestEngine/UnitTest/main.cpp
+++ VestEngine/UnitTest/main.cpp
@@ -1,15 +1,66 @@
+#include <functional>
+#include <iostream>
+
 #include "Utils/DenseArray.h"
 
-#include <iostream>
+std::vector<std::function<void()>> tests;
+
+#define TESTBEGIN(name) \
+class name { \
+public: \
+std::string label = #name; \
+name() { tests.push_back(std::bind(&name::test, this)); } \
+void test() \
+
+#define TESTEND(name) \
+}; \
+name name; \
+
+
+#define TEST(value, failMessage) \
+if (value) \
+{ \
+	std::cout << label << " succeeded." << std::endl; \
+} \
+else \
+{ \
+	std::cerr << label << " failed : " << failMessage << std::endl; \
+}
 
 int main()
 {
-	DenseArray<int> lol;
-	lol.initialize(5);
-	int* lol1 = lol.add();
-	*lol1 = 5;
+	TESTBEGIN(Test1_Capacity1)
+	{
+		DenseArray<int> ints;
+		ints.initialize(1);
 
-	std::cout << *lol1 << std::endl;
+		TEST(ints.getCapacity() == 1, "Capacity != 1")
+	}
+	TESTEND(Test1_Capacity1)
+
+	TESTBEGIN(Test1_Capacity100)
+	{
+		DenseArray<int> ints;
+		ints.initialize(100);
+
+		TEST(ints.getCapacity() == 100, "Capacity != 100")
+	}
+	TESTEND(Test1_Capacity100)
+
+
+	TESTBEGIN(Test1_Capacity0)
+	{
+		DenseArray<int> ints;
+		ints.initialize(0);
+
+		TEST(ints.getCapacity() == 0, "Capacity != 0")
+	}
+	TESTEND(Test1_Capacity0)
+
+	for (auto test : tests)
+	{
+		test();
+	}
 
 	return 0;
 }
