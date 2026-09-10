854b6ee2fdc7dddc726d6d35371aa6fe92291df1
Etienne Champagne <etienne.champagne@dont-nod.com>
2026-09-10 16:21:34 +0200

    cleanup: tabs

--- VestEngine/Engine/Private/Managers/UIManager.cpp
+++ VestEngine/Engine/Private/Managers/UIManager.cpp
@@ -15,13 +15,13 @@ void UIManager::initialize()
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO();
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // IF using Docking Branch
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;	// Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;	// Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;		// IF using Docking Branch
 
 	// Setup Platform/Renderer backends
 	GLFWwindow* window = static_cast<GLFWwindow*>(platform::getWindowManager().getWindow());
-	ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+	ImGui_ImplGlfw_InitForOpenGL(window, true);				// Second param install_callback=true will install GLFW callbacks and chain to existing ones.
 	ImGui_ImplOpenGL3_Init();
 }
 
