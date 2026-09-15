1effc91836edf208b0efbd94e74835161e5fbb8b
Etienne Champagne <etienne.champagne.ec@gmail.com>
2026-09-15 15:03:59 +0200

    refacto: cmake v.studio / define

--- VestEngine/Editor/Public/Widgets/Components/MeshRendererComponentInspector.h
+++ VestEngine/Editor/Public/Widgets/Components/MeshRendererComponentInspector.h
@@ -6,7 +6,7 @@ class MeshRendererComponentInspector : public IInspectorElement
 {
 	unsigned int model = 0;
 	unsigned int shader = 0;
-	bool bOutline;
+	bool bOutline = false;
 
 public:
 	MeshRendererComponentInspector(Entity inEntity)
