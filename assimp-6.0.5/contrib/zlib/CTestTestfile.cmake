eba714101b2e4188425f6448eafb74b18b7e9f4e
Etienne Champagne <etienne.champagne.ec@gmail.com>
2026-09-21 16:17:00 +0200

    refacto: assimp is now imported through cmake fetchcontent

--- assimp-6.0.5/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/VestEngine/assimp-6.0.5/contrib/zlib
-# Build directory: D:/VestEngine/assimp-6.0.5/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
